cf19674357f540db8dc6459cc06a2b78f389c795
Kaleb Allen <kaleballen@interapts-macbook-pro.local>
2018-09-12 10:10:28 -0500

    Initial Commit

---
+++ Task Manager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task Manager
+//
+//  Created by Kaleb Allen on 9/12/18.
+//  Copyright © 2018 Kaleb Allen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
